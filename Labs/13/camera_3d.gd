88ca33471489213934ceb75797536513acd70810
Earvizu17 <edgar.arvizu@bsu.edu>
2024-11-14 00:59:04 -0500

    vfvf

---
+++ Labs/13/camera_3d.gd
@@ -0,0 +1,15 @@
+extends Camera3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		self.rotate_x(event.relative.y * 0.001)
